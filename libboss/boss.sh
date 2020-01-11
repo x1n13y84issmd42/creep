144c5e095dda140423f9cb127d2afa90e0b665f0
Eugene Merkulov <gaddeena@gmail.com>
2020-01-11 18:25:23 +0200

    Boss works.

--- libboss/boss.sh
+++ libboss/boss.sh
@@ -14,3 +14,27 @@ function boss.logg {
 function boss.loggg {
 	[[ ${CREEP_BOSS_LOG:-2} -ge 3 ]] && creep.echo "boss" $lcRed $@
 }
+
+BOSS_FILE=.creep/.boss
+
+function boss.load {
+	if [[ -f $BOSS_FILE ]]; then
+		readarray -t PROJECTS < $BOSS_FILE
+	else
+		runes.log "You don't have a .BOSS file. ${lcHint}Create a one with the ${lcCmd}creep/boss add PROJECT${lcHint} command to start managing."
+	fi
+}
+
+# Executes arbitrary commands within each project directory.
+function boss.execute {
+	IFS=''
+	local WD=$(pwd)
+	for PROJ in ${PROJECTS[@]}; do
+		PROJ=${PROJ##*\/}
+		cd $PROJ
+		boss.log "Doing in ${lcFile}$PROJ"
+		local OUT=$(eval "$@")
+		[[ ! -z $OUT ]] && echo ${OUT[@]}
+		cd $WD
+	done
+}
